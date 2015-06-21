cba52166dad5889fbe2857e81d16bf2b5f4bc01b
Ilia <grayfall9@me.com>
2015-06-21 14:05:20 +0300

    Finished a sketch of the llist.h header file.

--- clib/repeatevolver/evolver.h
+++ clib/repeatevolver/evolver.h
@@ -1,13 +1,49 @@
 //
-//  evolver.h
+//  llist.h
 //  repeatevolver
-//
 //  Created by Ilya Korvigo on 20.06.15.
-//  Copyright (c) 2015 ARRIAM. All rights reserved.
 //
 
-#ifndef repeatevolver_evolver_h
-#define repeatevolver_evolver_h
+#ifndef evolver_h
+#define evolver_h
+
+
+#include "gsl/gsl_rng.h"
+
+
+///////////////////////////
+//////// Typedefs  ////////
+///////////////////////////
+
+
+typedef struct Individual {
+    uint rep_left;  //replications left
+    char *sequence; // dna
+} Individual;
+
+
+///////////////////////////
+//////// Functions ////////
+///////////////////////////
+
+
+Individual* replicate_individual(Individual* individual) {
+    /*
+     Returns an individual that evolved from the given individual
+     */
+    
+    return 0;
+};
+
+
+char* extract_sequence_from_individual(Individual* individual) {
+    /*
+     Extracts a nucleotide sequence from an individual
+     */
+    
+    return 0;
+};
+
 
 
 #endif
