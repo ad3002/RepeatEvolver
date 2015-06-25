4cf93a10c42bd23329dd029c2f91c43269a73beb
Ilia <grayfall9@me.com>
2015-06-26 01:56:55 +0300

    Figured out threads

--- repeatevolver/repeatevolver/workflow.h
+++ repeatevolver/repeatevolver/workflow.h
@@ -19,6 +19,12 @@
 ///////////////////////////
 
 
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
 struct Node;
 typedef struct Node {
     struct Node* next;
@@ -29,8 +35,9 @@ typedef struct Node {
 typedef struct Queue {
     Node* head;
     Node* tail;
+    unsigned long size;
     pthread_mutex_t queue_lock;
-    pthread_cond_t condition;
+    pthread_cond_t  condition;
 } Queue;
 
 
@@ -49,85 +56,102 @@ typedef struct ThreadArgs {
 
 
 int pop_data(Queue* queue) {
-    assert(queue->head);
+    assert(queue->size);
     int data = queue->head->data;
+    Node* current_head = queue->head;
     if (queue->head->next) {
         queue->head = queue->head->next;
     } else {
         queue->head = NULL;
+        queue->tail = NULL;
     }
+    --queue->size;
+    free(current_head);
     return data;
 }
 
 void push_data(Queue* queue, int data) {
-    assert(data);
-    Node* new_node = malloc(sizeof *new_node);
-    assert(new_node);
-    new_node->data = 0;
-    new_node->next = NULL;
-    if (queue->head && queue->tail) {
-        queue->tail->next = new_node;
-        queue->tail = new_node;
+    Node* node = malloc(sizeof *node);
+    assert(node);
+    node->data = data;
+    node->next = NULL;
+    if (!queue->size) {
+        queue->head = node;
+        queue->tail = node;
     } else {
-        queue->head = new_node;
-        queue->tail = new_node;
+        queue->tail->next = node;
+        queue->tail = node;
     }
+    queue->size++;
 }
 
-void* worker(void* args) {
-    ThreadArgs* thread_args = (ThreadArgs* )args;
-    Queue* queue = thread_args->queue;
+
+void* worker_thread(void* args) {
+    ThreadArgs* thread_args = args;
     ThreadController* thread_controller = thread_args->thread_controller;
-    // Locking the queue
+    Queue* queue = thread_args->queue;
+    
     pthread_mutex_lock(&queue->queue_lock);
-    while (!queue->head) {
+    while (!queue->size) {
         pthread_cond_wait(&queue->condition, &queue->queue_lock);
     }
+    
     int data = pop_data(queue);
-    if (data == thread_args->goal) {
-        exit(0); // We're done
+    printf("Worker popped %d\n", data);
+    
+    if (data >= thread_args->goal) {
+        exit(0);
     }
-    for (int i=0; i<2; i++) {
-        push_data(queue, data+i+1);
+    pthread_mutex_unlock(&queue->queue_lock);
+    
+    // Processing data...
+    
+    pthread_mutex_lock(&queue->queue_lock);
+    for (int i = 1; i < 3; i++) {
+        push_data(queue, data+(10*i));
+        printf("Worker pushed %d\n", data+(10*i));
     }
     pthread_cond_signal(&queue->condition);
-    // Unlocking queue
     pthread_mutex_unlock(&queue->queue_lock);
-    // Updating thread_controller counter
+    
     pthread_mutex_lock(&thread_controller->controller_lock);
     thread_controller->threads_available++;
     pthread_cond_signal(&thread_controller->condition);
     pthread_mutex_unlock(&thread_controller->controller_lock);
-    pthread_exit(0);
+    pthread_exit(NULL);
 }
 
 
 void* boss_thread(void* args) {
-    ThreadArgs* thread_args = (ThreadArgs* )args;
+    ThreadArgs* thread_args = args;
     ThreadController* thread_controller = thread_args->thread_controller;
     pthread_t thread;
-    pthread_attr_t attr;
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
     int counter = 0;
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
     while (1) {
         pthread_mutex_lock(&thread_controller->controller_lock);
         while (!thread_controller->threads_available) {
-            // If number of threads is maxed out
-            pthread_cond_wait(&thread_controller->condition,
-                              &thread_controller->controller_lock);
+            pthread_cond_wait(&thread_controller->condition, &thread_controller->controller_lock);
         }
-        pthread_create(&thread, &attr, worker, args);
-        thread_controller->threads_available--;
-        counter++;
-        printf("%d\n", counter);
+        if (!pthread_create(&thread, &attr, worker_thread, args)) {
+            pthread_detach(thread);
+            thread_controller->threads_available--;
+            printf("Started thread # %d\n", ++counter);
+        }
         pthread_mutex_unlock(&thread_controller->controller_lock);
     }
     pthread_exit(NULL);
 }
 
+
 int main() {
     Queue queue;
-    push_data(&queue, 0);
+    queue.size = 0;
+    queue.head = NULL;
+    queue.tail = NULL;
+    push_data(&queue, 1);
     pthread_mutex_init(&queue.queue_lock, NULL);
     pthread_cond_init(&queue.condition, NULL);
     
@@ -137,16 +161,15 @@ int main() {
     pthread_cond_init(&controller.condition, NULL);
     
     ThreadArgs args;
-    args.goal = 100;
-    args.queue = &queue;
+    args.goal = 1000;
     args.thread_controller = &controller;
+    args.queue = &queue;
     
     pthread_t thread;
     pthread_create(&thread, NULL, boss_thread, &args);
     pthread_join(thread, NULL);
     return 0;
 }
-
 ///////////////////////////
 //////// Functions ////////
 ///////////////////////////
